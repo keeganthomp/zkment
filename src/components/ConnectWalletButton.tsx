9c3d77572f0c284f27e90c8ad3b68e8cacd1b14d
Keegan Thompson <whereiskeegan@gmail.com>
2024-09-16 17:59:50 -0500

    fixes

--- src/components/ConnectWalletButton.tsx
+++ src/components/ConnectWalletButton.tsx
@@ -2,10 +2,67 @@ import { useWalletModal } from "@solana/wallet-adapter-react-ui";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { formatAddress } from "@/utils/solana";
 import { useTokenBalances } from "@/context/tokenBalancesContext";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverAnchor,
+} from "@/components/ui/popover";
+
+const WalletInfo = ({
+  connectedWallet,
+  balance,
+  onPress,
+}: {
+  connectedWallet: string;
+  balance: string | number;
+  onPress: () => void;
+}) => {
+  return (
+    <button
+      onClick={onPress}
+      className="flex flex-col justify-center items-center gap-0 px-4 bg-gray-50 rounded-md cursor-pointer hover:bg-gray-100 w-full"
+    >
+      <p className="font-light text-gray-700">
+        {formatAddress(connectedWallet)}
+      </p>
+      <p className="text-sm font-thin text-gray-500 relative top-[-5px]">
+        {balance} SOL
+      </p>
+    </button>
+  );
+};
+
+const WalletPopoverMenu = () => {
+  const { disconnect, publicKey: connectedWallet } = useWallet();
+
+  const handleCopyAddress = () => {
+    if (connectedWallet) {
+      navigator.clipboard.writeText(connectedWallet.toBase58());
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-0 items-start justify-start rounded">
+      <button
+        onClick={handleCopyAddress}
+        className="h-9 px-3 w-full text-left text-xs font-semibold cursor-pointer hover:bg-gray-50"
+      >
+        Copy Address
+      </button>
+      <button
+        onClick={disconnect}
+        className="h-9 px-3 w-full text-left text-xs font-semibold cursor-pointer hover:bg-gray-50"
+      >
+        Disconnect
+      </button>
+    </div>
+  );
+};
 
 const ConnectWalletButton = () => {
   const { setVisible } = useWalletModal();
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const { publicKey: connectedWallet } = useWallet();
   const { solBalance, fetchSolBalance } = useTokenBalances();
   const hasCalledFetchBalance = useRef(false);
@@ -24,18 +81,32 @@ const ConnectWalletButton = () => {
 
   if (connectedWallet) {
     return (
-      <div className="flex flex-col justify-center items-center gap-0 px-4">
-        <p className="font-light text-gray-700">{formatAddress(connectedWallet.toBase58())}</p>
-        <p className="text-sm font-thin text-gray-500 relative top-[-5px]">
-          {solBalance} SOL
-        </p>
-      </div>
+      <Popover
+        open={isPopoverOpen}
+        onOpenChange={() => setIsPopoverOpen(false)}
+      >
+        <PopoverAnchor className="flex items-center justify-center">
+          <WalletInfo
+            connectedWallet={connectedWallet.toBase58()}
+            balance={solBalance}
+            onPress={() => setIsPopoverOpen(true)}
+          />
+        </PopoverAnchor>
+        <PopoverContent className="w-36 p-0">
+          <WalletPopoverMenu />
+        </PopoverContent>
+      </Popover>
     );
   }
 
   return (
     <div className="flex flex-col justify-center items-center px-4">
-      <button onClick={() => setVisible(true)}>Connect Wallet</button>
+      <button
+        className="bg-gray-700 h-10 rounded-md text-white text-sm w-full"
+        onClick={() => setVisible(true)}
+      >
+        Connect Wallet
+      </button>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import "./index.css";
+import '@solana/wallet-adapter-react-ui/styles.css'
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
