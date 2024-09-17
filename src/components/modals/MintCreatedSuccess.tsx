457fe25808d54a65b85b63e7b68d26def659f233
Keegan Thompson <whereiskeegan@gmail.com>
2024-09-17 09:36:51 -0500

    copy new mint

--- src/components/modals/MintCreatedSuccess.tsx
+++ src/components/modals/MintCreatedSuccess.tsx
@@ -2,12 +2,12 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 import { openExplorerUrl } from "@/utils/solana";
+import { Copy } from "lucide-react";
 
 export function MintCreatedModal({
   isOpen,
@@ -18,21 +18,28 @@ export function MintCreatedModal({
   onClose: () => void;
   mintAddress: string;
 }) {
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(mintAddress);
+  };
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Mint Created!</DialogTitle>
         </DialogHeader>
-        <DialogDescription className="font-thin">
-            <div>
-          <p>Your mint has been successfully created and is now available.</p>
+        <div className="font-thin text-sm">
+          <div>
+            <p>Your mint has been successfully created and is now available.</p>
           </div>
           <div className="pt-5 text-gray-600 text-center">
-            <p>Mint Address</p>
-            <p className="cursor-pointer underline" onClick={() => openExplorerUrl(mintAddress)}>{mintAddress}</p>
+            <p className="font-semibold">Mint Address</p>
+            <div className="flex items-center gap-1" onClick={copyToClipboard}>
+              <p className="cursor-pointer">{mintAddress}</p>
+              <Copy className="cursor-pointer" width={15} />
+            </div>
           </div>
-        </DialogDescription>
+        </div>
         <DialogFooter>
           <Button onClick={onClose}>Close</Button>
         </DialogFooter>
